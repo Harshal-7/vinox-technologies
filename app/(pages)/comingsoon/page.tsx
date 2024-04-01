eb788d18d268740912cdff76ee2e3eb7a54f3e5d
Harshal Shinde <shindeharshal009@gmail.com>
2024-04-01 14:01:39 +0530

    comingsoom page added

---
+++ app/(pages)/comingsoon/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { Lilita_One } from "next/font/google";
+import { cn } from "@/lib/utils";
+import React, { useRef, useEffect } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { TextPlugin } from "gsap/TextPlugin";
+
+const font = Lilita_One({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+const ComingSoon = () => {
+  const container1 = useRef(null);
+  const container2 = useRef(null);
+  gsap.registerPlugin(TextPlugin); // Register the TextPlugin
+
+  useGSAP(() => {
+    gsap.from(container1.current, {
+      duration: 1,
+      text: {
+        value: "sfsddf fdss a!",
+      },
+    });
+    gsap.from(container2.current, {
+      duration: 1.5,
+      text: {
+        value: "sfsddf fdss a!",
+      },
+    });
+  });
+
+  return (
+    <div
+      className={cn(
+        "flex flex-col gap-10 items-center justify-center w-full h-screen bg-red-600",
+        font.className
+      )}
+    >
+      <div ref={container1} className=" text-9xl font-bold text-yellow-400">
+        Coming Soon !!
+      </div>
+      <p ref={container2} className=" text-4xl text-yellow-400">
+        We're working on something awesome! Stay tuned...
+      </p>
+    </div>
+  );
+};
+
+export default ComingSoon;

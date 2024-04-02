6e3c9a2e36215fe36776bf186b969f97de93790e
Harshal Shinde <shindeharshal009@gmail.com>
2024-04-02 20:15:35 +0530

    vercel deployment error fixed - _introduction changed to  Introduction.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Heading } from "./_components/heading";
 import { Heros } from "./_components/heros";
 import { Footer } from "./_components/footer";
 import { Navbar } from "./_components/navbar";
-import { Introduction } from "./(pages)/_introduction/page";
+import { Introduction } from "./(pages)/introduction/Introduction";
 
 export default function Home() {
   return (
